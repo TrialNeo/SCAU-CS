c8c78b86a6406f0a8736fd4e5b8abafe836d1b00
Trial <shenpanpro@gmail.com>
2026-03-12 13:12:44 +0800

    19669 & 19670

---
+++ exp2/19669-在显示屏上显示指定字符串.c
@@ -0,0 +1,14 @@
+// Created by Github@TrialNeo(shenpanpro@gmail.com) 
+// Created Time 2026/3/12.
+
+
+
+/*
+    要求编写一个程序，在显示屏上显示如下内容（全为半角字符，且不包含空格）：
+    c:\npc\readme.txt
+    [提示] 注意转义字符在程序中的使用
+ */
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+    printf("c:\\npc\\readme.txt");
+}
---
+++ exp2/19670-基本运算，写出程序运行结果.c
@@ -0,0 +1,14 @@
+// Created by Github@TrialNeo(shenpanpro@gmail.com) 
+// Created Time 2026/3/12.
+
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+    int a,b,c;
+    double d=15,e,f;
+    a = 35%7;
+    b= 15/10;
+    c=  b++;
+        e= 15/10;
+    f = d/10;
+    printf("%d,%d,%d,%lf,%lf,%lf", a, b, c, d, e, f);
+}
