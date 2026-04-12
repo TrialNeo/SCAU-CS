038d8b0d5dd0a687a2be486b9531846445b156fe
Trial <shenpanpro@gmail.com>
2026-04-12 11:57:19 +0800

    update: fix .gitignore

--- leetcode/2026-04-12/326-isPowerOfThree.cpp
+++ leetcode/2026-04-12/326-isPowerOfThree.cpp
@@ -3,17 +3,20 @@ using namespace std;
 #include <math.h>
 class Solution {
 public:
-    bool isPowerOfThree(int n) {}
+    bool isPowerOfThree(int n) { return n > 0 && 1162261467 % n == 0; }
 };
 
 
 int main(int argc, char const *argv[]) {
     // 手动找一下最大的数
-    int n = 1;
-    int border = pow(2, 31);
-    while (-border < n && border - 1) {
-        n *= 3;
-    }
-    
+    // int n = 1;
+    // long border = pow(2, 31);
+    // while (-border < n * 3 && n * 3 < border - 1) {
+    //     n *= 3;
+    // }
+    // cout << n;
+    Solution s;
+
+    cout << s.isPowerOfThree(45);
     return 0;
 }
